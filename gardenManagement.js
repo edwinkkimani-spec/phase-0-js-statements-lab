09729598170d3bbb18a1487753c1a46184ab8025
edwinkkimani-spec <edwinkkimani@gmail.com>
2026-09-25 17:05:46 +0300

    update project

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
 const temperature = 90;
+if(temperature>80) {
+    console.log("Watering on")
+} else{
+ console.log("Water off");
+}
+ 
 const timeOfDay = "morning";
-let soilMoisture = 30;
+if(timeOfDay === "morning"){
+    console.log("Lights off");
+} else{
+  console.log("Lights on");
+}
+
+let soilMoisture =10;
+while(soilMoisture<40){
+    soilMoisture +=5
+    console.log(soilMoisture);
+
+}
